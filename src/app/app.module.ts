afac410bf74751ee3de2ba16207a3b4c97dd2f8a
Ruben Soler <r.solerginer@gmail.com>
2017-09-04 16:51:51 +0200

    universal prerender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'ang-seo-pre'}),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template: '<h1>Home works!</h1>'
+})
+export class HomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
